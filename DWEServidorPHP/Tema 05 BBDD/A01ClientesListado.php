95fca6920f5ad992ded59829a3f2483336db65e2
Pablo Bustos <pabuspaz@gmail.com>
2020-10-19 21:11:41 +0200

    Actualizacion

---
+++ DWEServidorPHP/Tema 05 BBDD/A01ClientesListado.php
@@ -0,0 +1,35 @@
+<?php
+//conexion a la base de datos
+$link = new mysqli('localhost', 'root', '', 'virtualmarket'); 
+//comprobar errores de conexión
+if ( $link->connect_errno ){ 
+	echo "Fallo al conectar a MySQL: ". $link->connect_error; 
+} else
+//no hay errores de conexión
+{
+	// seleccionar charset
+	$link->set_charset('utf-8'); 
+	//crear string de la consulta
+	$consulta="SELECT * FROM clientes ";
+	//ejecutar la consulta
+	$result=$link->query($consulta);
+    //recorremos el resultado
+    echo "<table border='1'>
+    <tr>
+    <td>DNI</td>
+    <td>nombre</td>
+    <td>direccion</td>
+    <td>email</td>
+    <td>pwd</td>
+    </tr>";
+	while ($fila=$result->fetch_assoc()){
+        echo "<tr>";
+        foreach ($fila as $value){
+            echo "<td>$value</td>";
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
+	$result->free();
+	$link->close(); 
+}
