00a4e637b985499dd7139d08281dd17485f9af61
Pablo Bustos <pabuspaz@gmail.com>
2020-10-21 02:43:37 +0200

    Actu

--- DWEServidorPHP/ActividadesPorDistribuir/ArrayEjer2-2.php
+++ DWEServidorPHP/ActividadesPorDistribuir/ArrayEjer2-2.php
@@ -10,12 +10,13 @@
 	$Alemania = array ('Pais'=>Alemania,'Capital' => Berlin , 'Extension' =>57046, 'Habitantes'=> 78420000);
 	$Austria = array('Pais' =>Austria, 'Capital' => Viena, 'Extension' => 83849, 'Habitantes' => 7614000);
 	$Bélgica = array('Pais' =>Bélgica, 'Capital' => Bruselas, 'Extension' => 7614000, 'Habitantes' => 9932000);
-/**
+
+	/*
 	$Pais = array("Alemania", "Austria", "Bélgica");
 	$Capital = array("Berlin", "Viena", "Bruselas");
 	$Extension = array("57046", "83849", "30518");
 	$Habitantes = array("78420000", "7614000", "9932000");
-	**/
+	*/
 function tabla($array){
 	
 	foreach($Pais as $Paises){
