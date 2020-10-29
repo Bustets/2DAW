4895e9e36095c6654bf6a208d287ab8e6c34dc17
Pablo Bustos <pabuspaz@gmail.com>
2020-10-29 16:39:15 +0100

    Act

--- DWEclienteJS/UD07 AJAX/A03LLamadasJSonPHP-falla/ajaxphp.js
+++ DWEclienteJS/UD07 AJAX/A03LLamadasJSonPHP-falla/ajaxphp.js
@@ -3,14 +3,11 @@ function enviar_peticion(accion){
     var conexion = new XMLHttpRequest();
     conexion.onreadystatechange = function(){
         if (conexion.readyState == 4 && conexion.status == 200) {
-            document.getElementById("respuesta").innerText= conexion.responseText;
+            document.getElementById("respuesta").innerText= conexion.responseText
         }
-    }
+    };
 
-
-
-
-    conexion.open("POST","UD07-3AJAX.php", true);
+    conexion.open('POST','UD07-3AJAX.php', true);
     conexion.setRequestHeader("Content-type","appllication/x-www-form-urlencoded");
     conexion.send('accion=' +accion);
 
