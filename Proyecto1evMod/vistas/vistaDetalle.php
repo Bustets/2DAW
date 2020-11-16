4cbe77412a2fb66f918d1b2fb24dff623578983f
Pablo Bustos <pabuspaz@gmail.com>
2020-11-16 02:57:36 +0100

    Actual

--- Proyecto1evMod/vistas/vistaDetalle.php
+++ Proyecto1evMod/vistas/vistaDetalle.php
@@ -2,4 +2,4 @@
 
 echo "Bienvenido ".$_SESSION['nombre']."<br>";
 
-echo "<div style= 'width: 300px; display:inline-block; text-align:center;'><br><img src='../fotos/".$infoProducto['foto']."'width='100px'; height='120px';<br></div><div style= display:inline-block;>".$infoProducto['idProducto']."<br>".$infoProducto['nombre']."<br>".$infoProducto['origen']."<br>".$infoProducto['marca']."<br>".$infoProducto['categoria']."<br>".$infoProducto['peso']."<br>".$infoProducto['precio']."<br><form><input type=number value=1 min='1'><br><input type=submit value=Comprar></form></div>";
+echo "<div style= 'width: 300px; display:inline-block; text-align:center;'><br><img src='../fotos/".$infoProducto['foto']."'width='100px'; height='120px';<br></div><div style= display:inline-block;>".$infoProducto['idProducto']."<br>".$infoProducto['nombre']."<br>".$infoProducto['origen']."<br>".$infoProducto['marca']."<br>".$infoProducto['categoria']."<br>".$infoProducto['peso']."<br>".$infoProducto['precio']."<br><form method=GET action=verCarrito.php><input type=number value=1 min='1'><br><input type=submit value=Comprar></form></div>";
