4c14245d9ba685af3b0e62bda8fe093d8dbade4c
Pablo Bustos <pabuspaz@gmail.com>
2020-11-20 14:05:25 +0100

    actu

--- Proyecto1evMod/vistas/script.js
+++ Proyecto1evMod/vistas/script.js
@@ -2,26 +2,26 @@ $(document).ready(function () {
 
     $('.modalAnyadir').hide();
     $('.modalEditar').hide();
-    $("#anyadirCliente").click(function(){
+    $("#anyadirCliente").click(function () {
         $(".modalAnyadir").show();
-      });
-      $("#anyadirPedido").click(function(){
+    });
+    $("#anyadirPedido").click(function () {
         $(".modalAnyadir").show();
-      });
-     
+    });
 
-    $("#anyadirCli").click(function(){
+
+    $("#anyadirCli").click(function () {
         anyadirCliente();
     });
 
-      cargarClientes();
-      cargarPedidos();
-      borrarCliente();
-      editarCliente();
+    cargarClientes();
+    cargarPedidos();
+    borrarCliente();
+    editarCliente();
 
 });
-function cargarClientes(){
-      
+function cargarClientes() {
+
     $.ajax({
 
         url: '../controladores/getClientes.php',
@@ -50,42 +50,42 @@ function cargarClientes(){
     });
 }
 
-function cargarPedidos(){
+function cargarPedidos() {
 
     $.ajax({
 
-      url: '../controladores/getPedidos.php',
+        url: '../controladores/getPedidos.php',
 
-      type: 'POST',
+        type: 'POST',
 
-      dataType: 'json',
+        dataType: 'json',
 
-      success: function (respuesta) {
-          console.log(respuesta);
-          for (var key in respuesta) {
-              $("#tabla_Pedidos").append("<tr><td class='idPedido'>" + respuesta[key].idPedido + "</td><td id='dni'>" + respuesta[key].dniCliente + "</td><td id='fecha'>" + respuesta[key].fecha + 
-              "</td> <td><button class='detallePedido'>Detalle</button><button class='editarPedido'>Editar</button><button class='borrarPedido'>Borrar</button></td></tr>");
-          }
-      },
+        success: function (respuesta) {
+            console.log(respuesta);
+            for (var key in respuesta) {
+                $("#tabla_Pedidos").append("<tr><td class='idPedido'>" + respuesta[key].idPedido + "</td><td id='dni'>" + respuesta[key].dniCliente + "</td><td id='fecha'>" + respuesta[key].fecha +
+                    "</td> <td><button class='detallePedido'>Detalle</button><button class='editarPedido'>Editar</button><button class='borrarPedido'>Borrar</button></td></tr>");
+            }
+        },
 
-      error: function (xhr, status) {
-          alert('Disculpe, existió un problema2');
-      }
-  });
+        error: function (xhr, status) {
+            alert('Disculpe, existió un problema2');
+        }
+    });
 }
 
- function anyadirCliente(){
-        var objeto_Cliente = {
-        dniCliente:$('#dni_anyadir').val(),
-        nombre:$('#nombre_anyadir').val(),
-        direccion:$('#direccion_anyadir').val(),
-        email:$('#email_anyadir').val(),
-        pwd:$('#pass_anyadir').val(),
-        administrador:$('#admin_anyadir').val(),
+function anyadirCliente() {
+    var objeto_Cliente = {
+        dniCliente: $('#dni_anyadir').val(),
+        nombre: $('#nombre_anyadir').val(),
+        direccion: $('#direccion_anyadir').val(),
+        email: $('#email_anyadir').val(),
+        pwd: $('#pass_anyadir').val(),
+        administrador: $('#admin_anyadir').val(),
     };
 
 
-        $.ajax({
+    $.ajax({
 
         url: '../controladores/insertar.php',
 
@@ -95,106 +95,106 @@ function cargarPedidos(){
 
         dataType: 'json',
 
-       
 
-    }).done(function(respuesta){ 
-       if(respuesta){
+
+    }).done(function (respuesta) {
+        if (respuesta) {
             alert("Dato insertado");
-            $("#tabla_Clientes").append("<tr><td class='dniCliente'>"+objeto_Cliente.dniCliente+"</td><td calass='nombreCli'>"+objeto_Cliente.nombre+"</td> <td><button id='editarCliente'>Editar</button><button class='borrarCliente'>Borrar</button></td></tr>");
+            $("#tabla_Clientes").append("<tr><td class='dniCliente'>" + objeto_Cliente.dniCliente + "</td><td calass='nombreCli'>" + objeto_Cliente.nombre + "</td> <td><button id='editarCliente'>Editar</button><button class='borrarCliente'>Borrar</button></td></tr>");
 
-        }else{ 
+        } else {
             alert("Error en la insercion");
-        } 
-    }).fail(function( jqXHR, textStatus, errorThrown ) {
-        console.log( "La solicitud ha fallado: " +  textStatus + errorThrown);
+        }
+    }).fail(function (jqXHR, textStatus, errorThrown) {
+        console.log("La solicitud ha fallado: " + textStatus + errorThrown);
     });
 
-    }
-    function abrirFormularioanyadir(){
-        $("#anyadirCli").click(function(){
-            anyadirCliente();
-        });
-    }
+}
+function abrirFormularioanyadir() {
+    $("#anyadirCli").click(function () {
+        anyadirCliente();
+    });
+}
 
-    function borrarCliente(){
-        $('.borrarCliente').click(function(){
-            var borrarFila=$(this).parent().parent();
-            var datos={dniCliente:borrarFila.find('.dniCliente').text(),};
-        
+function borrarCliente() {
+    $('.borrarCliente').click(function () {
+        var borrarFila = $(this).parent().parent();
+        var datos = { dniCliente: borrarFila.find('.dniCliente').text(), };
+
 
         $.ajax({
 
             url: '../controladores/borrar.php',
-    
+
             type: 'POST',
-    
+
             data: datos,
-    
+
             dataType: 'json',
 
-        }).done (function(respuesta){
-            if(respuesta){
+        }).done(function (respuesta) {
+            if (respuesta) {
                 alert("Cliente Borrado");
                 borrarFila.remove();
-            }else{
+            } else {
                 alert("Error Al Borrar");
             }
-        }).fail(function(jqXHR, textStatus, errorThrown){
-            console.log( "La solicitud ha fallado: " +  textStatus + errorThrown);
+        }).fail(function (jqXHR, textStatus, errorThrown) {
+            console.log("La solicitud ha fallado: " + textStatus + errorThrown);
         });
     });
 }
-function editarCliente(){
-    $(".editarCliente").click(function(){
+function editarCliente() {
+    $(".editarCliente").click(function () {
         $(".modalEditar").show();
-        var fila=$(this).parent().parent();
-        var dni=fila.find('.dniCliente').text(); 
+        var fila = $(this).parent().parent();
+        var dni = fila.find('.dniCliente').text();
         console.log(dni);
         $.ajax({
-            url: "../controladores/getClientes.php", 
+            url: "../controladores/getClientes.php",
             type: "POST",
             dataType: "json",
             success: function (respuestaDatos) {
                 console.log(respuestaDatos);
                 for (var key in respuestaDatos) {
-                    if (respuestaDatos[key].dniCliente==dni) {
+                    if (respuestaDatos[key].dniCliente == dni) {
                         $('#dniEditar').val(respuestaDatos[key].dniCliente);
                         $('#nombreEditar').val(respuestaDatos[key].nombre);
                         $('#direccionEditar').val(respuestaDatos[key].direccion);
                         $('#emailEditar').val(respuestaDatos[key].email);
-                        }
+                    }
                 }
-             },
+            },
             error: function (jqXHR, textStatus, errorThrown) {
-             console.log("La solicitud ha fallado: " + textStatus + errorThrown); //
-            }      
+                console.log("La solicitud ha fallado: " + textStatus + errorThrown); //
+            }
         });
-        $("#editCli").click(function(){
-                var objeto_mod = {  
-                    dniCliente:$('#dniEditar').val(),
-                    nombre:$('#nombreEditar').val(),
-                    direccion:$('#direccionEditar').val(),
-                    email:$('#emailEditar').val(),
-                    pwd:$('#passEditar').val(),
-                    administrador:$('#adminEditar').val(),
-                };    
-                $.ajax({
-                    url:"../controladores/modificar.php",
-                    type:"POST",
-                    data: objeto_mod,
-                    dataType:"json",
-                    }).done(function(respuesta){
-                    if(respuesta){
-                        alert("Dato actualizado");
-                        $(fila).after("<tr><td class='dniCliente'>"+objeto_mod.dniCliente+"</td><td calass='nombreCli'>"+objeto_mod.nombre+"</td> <td><button id='editarCliente'>Editar</button><button class='borrarCliente'>Borrar</button></td></tr>")
-                        $(fila).remove();
-                        $("#modalEditar").hide();
-                    }else{ 
-                        alert("Error al acctualizar");
-                    } 
-                    }).fail(function( jqXHR, textStatus, errorThrown ) {
-                    console.log( "La solicitud ha fallado: " +  textStatus + errorThrown);
-                });
+        $("#editCli").click(function () {
+            var objeto_mod = {
+                dniCliente: $('#dniEditar').val(),
+                nombre: $('#nombreEditar').val(),
+                direccion: $('#direccionEditar').val(),
+                email: $('#emailEditar').val(),
+                pwd: $('#passEditar').val(),
+                administrador: $('#adminEditar').val(),
+            };
+            $.ajax({
+                url: "../controladores/modificar.php",
+                type: "POST",
+                data: objeto_mod,
+                dataType: "json",
+            }).done(function (respuesta) {
+                if (respuesta) {
+                    alert("Dato actualizado");
+                    $(fila).after("<tr><td class='dniCliente'>" + objeto_mod.dniCliente + "</td><td calass='nombreCli'>" + objeto_mod.nombre + "</td> <td><button id='editarCliente'>Editar</button><button class='borrarCliente'>Borrar</button></td></tr>")
+                    $(fila).remove();
+                    $("#modalEditar").hide();
+                } else {
+                    alert("Error al acctualizar");
+                }
+            }).fail(function (jqXHR, textStatus, errorThrown) {
+                console.log("La solicitud ha fallado: " + textStatus + errorThrown);
             });
+        });
     });
 }
