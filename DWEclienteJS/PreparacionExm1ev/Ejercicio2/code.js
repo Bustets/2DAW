e27cd2c88dd8d325db8a6b77860499325f101e65
Pablo Bustos <pabuspaz@gmail.com>
2020-12-10 15:10:21 +0100

    Actualizacion preExamen

---
+++ DWEclienteJS/PreparacionExm1ev/Ejercicio2/code.js
@@ -0,0 +1,16 @@
+$(document).redy(function(){
+
+    console.log("juary ok");
+
+    cargo_preguntas()
+
+});
+
+
+function cargo_preguntas(){
+
+    ///llamada ajax, recojo preguntas.json
+    //con la respuesta itero
+    //el json para montar la pgrunta + radio buttons de respuesta
+    
+}
