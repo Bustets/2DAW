ab617ab2ef3e39aeba3f02219bf683ebc8c38593
Pablo Bustos <pabuspaz@gmail.com>
2021-01-25 19:44:16 +0100

    actu

--- DWEServidorPHP/Api/UtilizandoUnaApi/api.php
+++ DWEServidorPHP/Api/UtilizandoUnaApi/api.php
@@ -1,9 +1,6 @@
 <?php
-$nasa =json_decode(file_get_contents("https://api.nasa.gov/planetary/apod?api_key=sk1mKvLicboTTuGP8kDFIXa0V9Ay1nO0cTCa5zi1"));
-$tabla="<table style='border: solid black 1px'>";
-foreach ($nasa as $key => $value) {
-$tabla.="<tr> <td>$key</td> ";
-$tabla.="</tr>";
-}
-$tabla.="</table>";
-echo $tabla;
+$nasa =json_decode(file_get_contents("https://api.nasa.gov/planetary/apod?api_key=sk1mKvLicboTTuGP8kDFIXa0V9Ay1nO0cTCa5zi1"), true);
+//var_dump($nasa);
+
+    echo "Titulo:".$nasa['title']." <br>";
+    echo "<img src='".$nasa['url']."'> <br>";
