773d2b9db5721e345c64c1c9fe4372d4e645f003
Pablo Bustos <pabuspaz@gmail.com>
2021-02-12 18:18:14 +0100

    Actualizar

---
+++ Proyecto2evPHP/producto/producto.php
@@ -0,0 +1,73 @@
+<?php
+
+include "utils.php";
+include "modelo.php";
+
+$base= new Bd();
+
+/*
+  listar todos los posts o solo uno
+ */
+if ($_SERVER['REQUEST_METHOD'] == 'GET')
+{
+    if (isset($_GET['idProducto']))
+    {
+      //Mostrar un Producto
+      $producto= new Producto($_GET['idProducto'],'','','','','','','','','');
+      $dato=$producto->buscar($base->link);
+      header("HTTP/1.1 200 OK");
+      echo json_encode($dato);
+      exit();
+	  }
+    else {
+      //Mostrar lista de Producto
+      $dato=Producto::getAll($base->link);
+      $dato->setFetchMode(PDO::FETCH_ASSOC);
+      header("HTTP/1.1 200 OK");
+      echo json_encode($dato->fetchAll());
+      exit();
+	}
+}
+
+// Crear un nuevo post
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+    $producto= new Producto($_POST['idProducto'],$_POST['nombre'],$_POST['origen'],$_POST['foto'],$_POST['marca'],$_POST['categoria'],$_POST['peso'],$_POST['unidades'],$_POST['volumen'],$_POST['precio']);
+    if(!$producto->buscar($base->link)){
+      $producto->insertar($base->link);
+      header("HTTP/1.1 200 OK");
+      echo json_encode($_POST['idProducto']);
+      exit();
+	 }
+}
+
+//Borrar
+if ($_SERVER['REQUEST_METHOD'] == 'DELETE')
+{
+	$idProducto = $_GET['idProducto'];
+  $producto= new Producto($_GET['idProducto'],'','','','','','','','','');
+  if($dato=$producto->borrar($base->link)){
+	 header("HTTP/1.1 200 OK");
+   	 echo json_encode($idProducto);
+	 exit();
+  }
+}
+
+//Actualizar
+if ($_SERVER['REQUEST_METHOD'] == 'PUT')
+{
+
+  if(isset($_GET['idProducto'])){
+    $producto= new Producto($_GET['idProducto'],'','','','','','','','','');
+    $error=$producto->modificarParcial($base->link,$_GET);
+    header("HTTP/1.1 200 OK");
+    echo $_GET['dniCliente'];
+    exit();
+  }
+}
+
+
+//En caso de que ninguna de las opciones anteriores se haya ejecutado
+header("HTTP/1.1 400 Bad Request");
+
+?>
