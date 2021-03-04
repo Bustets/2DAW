71029250c71fd9dda2b1c871e35c95371a5aae96
Pablo Bustos <pabuspaz@gmail.com>
2021-03-04 17:02:18 +0100

    Actualizar

--- RepasoMarzo-Mayo/3ºEjerccioArray/Array.php
+++ RepasoMarzo-Mayo/3ºEjerccioArray/Array.php
@@ -1,20 +1,71 @@
 <?php
 
-/*$arrayHeader = ["País", "Capital", "Extension", "Habitantes"];
-$arrayAlemania = ["Alemania", "Berlin", "557046", "784200000"];
-$arrayAutria = ["Autria", "Viena", "83849", "7614000"];
-$arrayBelgica = ["Bélgica", "Bruselas", "30518", "9932000"];*/
+    /*$arrayHeader = ["País", "Capital", "Extension", "Habitantes"];
+    $arrayAlemania = ["Alemania", "Berlin", "557046", "784200000"];
+    $arrayAutria = ["Autria", "Viena", "83849", "7614000"];
+    $arrayBelgica = ["Bélgica", "Bruselas", "30518", "9932000"];*/
 
-$arrayHeader = ["País", "Capital", "Extension", "Habitantes"];
-$arrayAlemania = ["Alemania", "Berlin", "557046", "784200000"];
-$arrayAutria = ["Autria", "Viena", "83849", "7614000"];
-$arrayBelgica = ["Bélgica", "Bruselas", "30518", "9932000"];
+    echo "Ejercio 1 Array";
 
-    foreach ($arrayHeader as $headers) {
-        foreach ($arrayAlemania as $arrayAlemania){
-            echo "$arrayAlemania <br>"
+    $arrayHeader = array("País", "Capital", "Extension", "Habitantes");
+    $arrayAlemania = array("Alemania", "Berlin", "557046", "784200000");
+    $arrayAutria = array("Autria", "Viena", "83849", "7614000");
+    $arrayBelgica = array("Bélgica", "Bruselas", "30518", "9932000");
+
+
+
+        echo "<table>";
+        echo "<tr>"; 
+        foreach ($arrayHeader as $value) {
+            echo "<th>";    
+            echo $value;
+            echo "</th>";
         }
-        echo "$headers <br>";
-    }
+        echo "</tr>";
+        echo "<tr>";
+        foreach ($arrayAlemania as $value) {
+            echo "<td>";    
+            echo $value;
+            echo "</td>";
+        }
+        echo "</tr>";
+        foreach ($arrayAutria as $value) {
+            echo "<td>";    
+            echo $value;
+            echo "</td>";
+        }
+        echo "<tr>";
+        foreach ($arrayBelgica as $value) {
+            echo "<td>";    
+            echo $value;
+            echo "</td>";
+        }
+        echo "</tr>";
+        echo "</table>";
+
+
+
+        echo "Ejercicio 2 array";
+
+    
+        $arrayPaises = array("Pais"=>array("Alemania","Austria","Bélgica"), 
+                            "Capital"=>array("Berlin","Viena","Bruselas"), 
+                            "Extensión"=>array("557046","83849","30518"), 
+                            "Habitantes"=>array("78420000","7614000","9932000"));
+
+        var_dump($arrayPaises);
+       
+       /*
+        echo "<table>";
+        echo "<tr>"; 
+        echo "</tr>";
+        echo "</table>";*/
+
+        foreach($arrayPaises as $pais){
+            foreach($pais as $value){
+                echo "$dato";
+            }
+            echo "<br>";
+        }
 
 ?>
